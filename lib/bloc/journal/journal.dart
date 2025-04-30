df05ecca2f5a3d6a0814fc2acc143507809a7fa0
kiqiou <kuksare@gmail.com>
2025-04-30 11:27:03 +0300

    fix: fix journal data fromjson function

--- lib/bloc/journal/journal.dart
+++ lib/bloc/journal/journal.dart
@@ -2,20 +2,22 @@ import '../user/user.dart';
 
 class Session {
   final int courseId;
+  final String? courseName;
   final String date;
   final String sessionType;
   final MyUser student;
   final String status;
   final int grade;
 
-  Session({required this.courseId, required this.date, required this.sessionType, required this.grade, required this.student, required this.status,});
+  Session({required this.courseId, required this.date, required this.sessionType, required this.grade, required this.student, required this.status, this.courseName});
 
   factory Session.fromJson(Map<String, dynamic> json) {
     return Session(
-      courseId: json['session']['course'],
+      courseId: json['session']['course']['id'],
+      courseName: json['session']['course']['name'],
       date: json['session']['date'],
       sessionType: json['session']['type'],
-      student: MyUser.fromJson(json['student']),
+      student: MyUser(username: json['student']['username'], role: json['student']['role']['role'], id: json['student']['id']),
       status: json['status'],
       grade: json['grade'],
     );
--- lib/bloc/user/user.dart
+++ lib/bloc/user/user.dart
@@ -1,23 +1,24 @@
 import 'package:equatable/equatable.dart';
 
 class MyUser extends Equatable {
+  final int? id;
   final String username;
   final String role;
 
   const MyUser({
+    this.id,
     required this.username,
     required this.role,
   });
 
   factory MyUser.fromJson(Map<String, dynamic> json) {
-    final userJson = json['user'];
-    if (userJson == null) {
+    if (json['user'] == null) {
       print('❌ Ошибка: JSON не содержит user');
       return MyUser(username: '', role: '');
     }
     return MyUser(
-      username: userJson['username'] ?? '',
-      role: userJson['role']?['role'] ?? '',
+      username: json['user']['username'] ?? '',
+      role:json['user']['role']?['role'] ?? '',
     );
   }
 
