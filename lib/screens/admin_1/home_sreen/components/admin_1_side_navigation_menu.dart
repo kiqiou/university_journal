78d5c9db884a3bd69d5e6b55b43815e77018d55d
kiqiou <kuksare@gmail.com>
2025-05-19 16:35:53 +0300

    fix: side navigations menu ui

--- lib/screens/admin_1/home_sreen/components/admin_1_side_navigation_menu.dart
+++ lib/screens/admin_1/home_sreen/components/admin_1_side_navigation_menu.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:university_journal/bloc/auth/authentication_bloc.dart';
@@ -20,10 +22,10 @@ class _Admin1SideNavigationMenu extends State<Admin1SideNavigationMenu> {
   ];
 
   final List<String> _texts = [
-    'Журнал',
-    'Лекции',
-    'Практика',
-    'Семинары',
+    'Список преподавателей',
+    'Список дисциплин',
+    'Добавить преподавателя',
+    'Добавить дисциплину',
   ];
 
   bool _isExpanded = false;
@@ -33,146 +35,127 @@ class _Admin1SideNavigationMenu extends State<Admin1SideNavigationMenu> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      color: Colors.grey.shade300,
-      duration: const Duration(milliseconds: 300),
-      width: _isExpanded ? _expandedWidth : _collapsedWidth,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  setState(() {
-                    _isExpanded = !_isExpanded;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+    return GestureDetector(
+      onTap: () {
+        if (_isExpanded) {
+          setState(() {
+            _isExpanded = false;
+          });
+        }
+      },
+      behavior: HitTestBehavior.opaque,
+      child: AnimatedContainer(
+        color: Colors.grey.shade300,
+        duration: const Duration(milliseconds: 300),
+        width: _isExpanded ? _expandedWidth : _collapsedWidth,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _isExpanded
+                  ? const Text(
+                'МИТСО\nМеждународный\nУниверситет',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+                  : SizedBox(
+                height: 24,
+                width: 24,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  if (!_isExpanded)
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          _isExpanded = !_isExpanded;
+                        });
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+                        child: MyIconContainer(
+                          icon: Icons.menu,
+                          width: (_isExpanded ? 250 : 50),
+                        ),
+                      ),
+                    ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: _isExpanded
+                        ? Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Панель навигации',
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      ),
+                    )
+                        : Divider(
+                      height: 1,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: _icons.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Center(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+                            child: InkWell(
+                              onTap: () {
+                              },
+                              child: MyIconContainer(
+                                icon: _icons[index],
+                                width: (_isExpanded ? 250 : 50),
+                                text: _texts[index],
+                                withText: _isExpanded,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+                child: InkWell(
+                  onHover: (hovering) {
+                    setState(() {
+                      isHovered = hovering;
+                    });
+                  },
+                  onTap: () {
+                    context.read<AuthenticationBloc>().add(AuthenticationLogoutRequested());
+                    log('➡️ Состояние: ${context.read<AuthenticationBloc>().state}');
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //     builder: (context) => WelcomeScreen(),
+                    //   ),
+                    // );
+                  },
                   child: MyIconContainer(
-                    icon: Icons.menu,
+                    borderRadius: 100,
+                    icon: Icons.arrow_back,
                     width: (_isExpanded ? 250 : 50),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: _isExpanded
-                    ? Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Профиль',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                )
-                    : Divider(
-                  height: 1,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => WelcomeScreen(),
-                    ),
-                  );
-                },
-                child: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-                  builder: (context, state) {
-                    if (state.status == AuthenticationStatus.authenticated && state.user != null) {
-                      return MyIconContainer(
-                        icon: Icons.account_circle_outlined,
-                        width: (_isExpanded ? 250 : 50),
-                        withText: _isExpanded,
-                        text: state.user!.username,
-                      );
-                    } else if (state.status == AuthenticationStatus.unauthenticated) {
-                      return MyIconContainer(
-                        icon: Icons.account_circle_outlined,
-                        width: (_isExpanded ? 250 : 50),
-                        withText: _isExpanded,
-                        text: 'Гость',
-                      );
-                    } else {
-                      return MyIconContainer(
-                        icon: Icons.account_circle_outlined,
-                        width: (_isExpanded ? 250 : 50),
-                        withText: _isExpanded,
-                        text: 'Загрузка...',
-                      );
-                    }
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                child: _isExpanded
-                    ? Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Панель навигации',
-                    style: TextStyle(color: Colors.grey, fontSize: 16),
-                  ),
-                )
-                    : Divider(
-                  height: 1,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: _icons.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
-                      child: InkWell(
-                        onTap: () {},
-                        child: MyIconContainer(
-                          icon: _icons[index],
-                          width: (_isExpanded ? 250 : 50),
-                          text: _texts[index],
-                          withText: _isExpanded ? true : false,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
-            child: InkWell(
-              onHover: (hovering) {
-                setState(() {
-                  isHovered = true;
-                });
-              },
-              onTap: () {
-
-              },
-              child: MyIconContainer(
-                borderRadius: 100,
-                icon: Icons.arrow_back,
-                width: (_isExpanded ? 250 : 50),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
