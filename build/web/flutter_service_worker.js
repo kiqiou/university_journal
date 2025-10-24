3ca5ae72df0d99b6c598b87fc56575a158c292b3
kiqiou <kuksare@gmail.com>
2025-10-24 11:31:22 +0300

    Add Flutter Web build for deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "513b459ba8cd3bde079feb6b99407de2
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3970de4b7b2b770c7d976365b1e7e52a",
+"flutter_bootstrap.js": "0821ccce1d910cca25d3446e1bb4cccf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
