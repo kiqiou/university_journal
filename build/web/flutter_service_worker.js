1f50fe245316c235c10803f6d49af86507d4b031
kiqiou <kuksare@gmail.com>
2025-10-25 11:43:53 +0300

    fix: fixed menu

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "513b459ba8cd3bde079feb6b99407de2
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a2c463d3eff61d2f2620e7c34e41e747",
+"flutter_bootstrap.js": "6a884a84cd4e30c0ae483b087ac94c14",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
