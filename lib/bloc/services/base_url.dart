dda3aab4ca8259de88cd8ed5c31d8889d226dc5b
kiqiou <kuksare@gmail.com>
2026-02-18 09:07:17 +0300

    new link for backend

--- lib/bloc/services/base_url.dart
+++ lib/bloc/services/base_url.dart
@@ -1,3 +1,4 @@
-final baseUrl = 'https://university-journal-back.onrender.com';
+final baseUrl = 'https://university-journal-back-1.onrender.com';
 //http://127.0.0.1:8000
 //https://university-journal-back.onrender.com
+//https://university-journal-back-1.onrender.com
