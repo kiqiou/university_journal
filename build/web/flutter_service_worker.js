bfcb6259d3457902b8a70947f8f7614e68274bbd
kiqiou <kuksare@gmail.com>
2026-03-03 12:19:00 +0300

    -

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "513b459ba8cd3bde079feb6b99407de2
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "78c975720ea6fd0061d3edc276a00040",
+"flutter_bootstrap.js": "844e88fc3bdef1f858c7b609be1682ab",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
