3acfb260964c708a8d693c37a75f381c141d4024
kiqiou <kuksare@gmail.com>
2026-03-03 11:18:45 +0300

    fix: changed dafaultValue for base url

--- lib/bloc/services/base_url.dart
+++ lib/bloc/services/base_url.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://'
+    defaultValue: 'https://university-journal-back.onrender.com'
 );
 //http://127.0.0.1:8000
 //https://university-journal-back.onrender.com
