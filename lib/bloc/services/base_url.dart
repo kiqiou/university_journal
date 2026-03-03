bd2989c3a8cf6994516c858f0131354c2096402c
kiqiou <kuksare@gmail.com>
2026-03-03 11:48:56 +0300

    ff

--- lib/bloc/services/base_url.dart
+++ lib/bloc/services/base_url.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final String baseUrl = dotenv.get(
-    'API_BASE_URL',
-    fallback: 'https://university-journal-back.onrender.com'
-);
+final String baseUrl = 'https://university-journal-back.onrender.com';
+
 //http://127.0.0.1:8000
 //https://university-journal-back.onrender.com
 
